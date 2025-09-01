4e82deca8fc4405d7ef537f925a71a916d656144
srirakshaprabhu <sriraksha.22cs160@sode-edu.in>
2025-09-01 10:41:48 +0530

    Update lab3.ino

--- lab3.ino
+++ lab3.ino
@@ -1,26 +1,29 @@
-// C++ code
-//
-int soil_sensor=0;
+
+int Smoke_Sensor=0;
 void setup()
 {
-  pinMode(A0, INPUT);
+  pinMode(A0,INPUT);
   Serial.begin(9600);
-   pinMode(13, OUTPUT);
-   
-  
-  
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 void loop()
 {
-  soil_sensor=analogRead(800);
-    Serial.println(soil_sensor);
-  if(soil_sensor>=800){
-    
-  digitalWrite(13, HIGH);
-  }else{
-    
- 
-  digitalWrite(13, LOW);
+  Smoke_Sensor=analogRead(A0);
+  Serial.println(Smoke_Sensor);
+  if(Smoke_Sensor>=80){
+    digitalWrite(7,HIGH);
+  }
+  else{
+    digitalWrite(7,LOW);
+  }
+  if(Smoke_Sensor>=100){
+    digitalWrite(8,HIGH);
+  }
+  else{
+    digitalWrite(8,LOW);
+  delay(10);
   }
 }
+     
